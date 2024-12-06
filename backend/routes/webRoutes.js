a54565b8547fb7537179d9a95e1a5c3ae9ff5b47
Muhammad Hamza Edhi <abdulanis190@gmail.com>
2024-12-06 20:23:42 +0500

    issue in tracking

--- backend/routes/webRoutes.js
+++ backend/routes/webRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {getTopRestaurants} = require('../controllers/topController');
 const {getRestaurants,getSpecificRestaurant,getRestaurantMenu, getReviews} = require('../controllers/restaurantController');
-const {getMenu,loginUser,registerUser} = require('../controllers/userController');
+const {getMenu,loginUser,registerUser,isEmailUnique} = require('../controllers/userController');
 const { handleSendOTP,handleVerifyOTP } = require('../services/emailService');
 
 
@@ -10,6 +10,7 @@ router.get('/home',getTopRestaurants);
 router.get('/restaurants',getRestaurants);
 router.get('/restaurants/:id',getRestaurantMenu);
 router.post('/login',loginUser);
+router.get('/is-email-unique',isEmailUnique)
 router.post('/register',registerUser);
 router.get('/menu/:id',getMenu);
 router.get('/getRestaurant/:id',getSpecificRestaurant);
